555409a15744ef8eff640edafc83d1891c0d3a53
Vitaly <kazancevvitallijj@gmail.com>
2020-10-05 13:46:13 +0800

    Ввод данных от пользователя

--- python_basics_lesson8/homework_pb8_task4_5_6.py
+++ python_basics_lesson8/homework_pb8_task4_5_6.py
@@ -59,7 +59,8 @@ class WareHouse:
                     new_dict = {'Количество шт.': quant - quantity}
                     el.update(new_dict)
                     break
-            break
+                else:
+                    break
         # return WareHouse(type_=type_, name=name, date=date, quantity=quant - quantity).receiving()
         return f'Выдан\n {type_} {json.dumps(give_out_dict, indent=4, sort_keys=True, ensure_ascii=False)}'
 
@@ -133,30 +134,80 @@ class Xerox(OfficeEquipment):
         return f'Ксерокс - \n{json.dumps(self.xerox_dict(), indent=4, sort_keys=True, ensure_ascii=False)}'
 
 
-p_1 = Printer('Brother', 'HL-1202R', 'A4', 'лазерная печать', False)
-print(p_1)
-position_1 = WareHouse('Принтер', p_1.printer_dict(), '03-10-2020', 30)
-print(position_1)
-p_2 = Printer('Epson', 'L-132', 'A4', 'струйный', True)
-position_2 = WareHouse('Принтер', p_2.printer_dict(), '03-10-2020', 20)
-print('-' * 100)
-print(p_2)
-print(position_2)
-print('-' * 100)
-s_1 = Scanner('Canon', 'CanoScan Lide 300', 'A4', 'планшетный')
-print(s_1)
-position_3 = WareHouse('Сканер', s_1.scanner_dict(), '04-10-2020', 15)
-print(position_3)
-x_1 = Xerox('Xerox', 'B205', 'A4', False)
-position_4 = WareHouse('Ксерокс', x_1.xerox_dict(), '04-10-2020', 5)
-print('-' * 100)
-print(x_1)
-print(position_4)
+while True:
+    res = input('Новые поступления? Да или нет:\n').lower()
+    if res == 'да':
+        tip = input('Что будем получать? Принтер, сканер или кссерокс?\n').title()
+        if tip == 'Принтер':
+            pr = Printer(name=input('Название принтера:\n'),
+                         model=input('Модель принтера:\n'),
+                         paper_format=input('Формат печати:\n'),
+                         way_of_print=input('Cпособ печати:\n'),
+                         color_print=input('Цветная печать, True или False:\n'))
+            position = WareHouse(tip, pr.printer_dict(), date=input('Дата поступления в формате дд-мм-гггг:\n'),
+                                 quantity=int(input('Сколько товара получили. Количество шт.:\n')))
+            print(pr)
+            print(position)
+        elif tip == 'Сканер':
+            sc = Scanner(name=input('Название сканера:\n'),
+                         model=input('Модель сканера:\n'),
+                         paper_format=input('Формат печати:\n'),
+                         kind=input('Тип сканера:\n'))
+            position = WareHouse(tip, sc.scanner_dict(), date=input('Дата поступления в формате дд-мм-гггг:\n'),
+                                 quantity=int(input('Сколько товара получили. Количество шт.:\n')))
+            print(sc)
+            print(position)
+        elif tip == 'Ксерокс':
+            xr = Xerox(name=input('Название ксерокса:\n'),
+                       model=input('Модель ксерокса:\n'),
+                       paper_format=input('Формат печати:\n'),
+                       color_print=input('Цветная печать, True или False:\n'))
+            position = WareHouse(tip, xr.xerox_dict(), date=input('Дата поступления в формате дд-мм-гггг:\n'),
+                                 quantity=int(input('Сколько товара получили. Количество шт.:\n')))
+            print(xr)
+            print(position)
+        else:
+            print(f'Склад принимает только принтеры, сканеры и ксерксы. Везите совй {tip} обратно.')
+    elif res == 'нет':
+        res = input('Будем что-нибудь выдавать? Да или нет:\n').lower()
+        if res == 'да':
+            give_out = WareHouse.give_out(type_=input('Что выдаем? Принтер, сканер или ксерокс?\n').title(),
+                                          name=input('Наименование выданной техники:\n'),
+                                          model=input('Модель выданной техники:\n'),
+                                          quantity=int(input('Количество выданной техники:\n')),
+                                          date=input('Дата выдачи в формате дд-мм-гггг:\n'))
+            print(give_out)
+            print('на складе хранится:\n' + '[' + '\n'.join(map(str, WareHouse.storage_list)) + ']')
+        elif res == 'нет':
+            break
+        pass
+    pass
+    # p_1 = Printer('Brother', 'HL-1202R', 'A4', 'лазерная печать', False)
+    # print(p_1)
+    # position_1 = WareHouse('Принтер', p_1.printer_dict(), '03-10-2020', 30)
+    # print(position_1)
+    # p_2 = Printer('Epson', 'L-132', 'A4', 'струйный', True)
+    # position_2 = WareHouse('Принтер', p_2.printer_dict(), '03-10-2020', 20)
+    # print('-' * 100)
+    # print(p_2)
+    # print(position_2)
+    # print('-' * 100)
+    # s_1 = Scanner('Canon', 'CanoScan Lide 300', 'A4', 'планшетный')
+    # print(s_1)
+    # position_3 = WareHouse('Сканер', s_1.scanner_dict(), '04-10-2020', 15)
+    # print(position_3)
+    # x_1 = Xerox('Xerox', 'B205', 'A4', False)
+    # position_4 = WareHouse('Ксерокс', x_1.xerox_dict(), '04-10-2020', 5)
+    # print('-' * 100)
+    # print(x_1)
+    # print(position_4)
+    #
+    # give_out = WareHouse.give_out('Принтер', 'Brother', 'HL-1202R', 5, '05-10-2020')
+    # print(give_out)
+    # print('на складе хранится:\n' + '[' + '\n'.join(map(str, WareHouse.storage_list)) + ']')
+    # give_out2 = WareHouse.give_out('Сканер', 'Canon', 'CanoScan Lide 300', 3, '05-10-2020')
+    # print(give_out2)
+    # print('на складе хранится:\n' + '[' + '\n'.join(map(str, WareHouse.storage_list)) + ']')
 
-give_out = WareHouse.give_out('Принтер', 'Brother', 'HL-1202R', 5, '05-10-2020')
-print(give_out)
-print('на складе хранится:\n' + '[' + '\n'.join(map(str, WareHouse.storage_list)) + ']')
-
-
-# 8 сделать ввод данных от пользователя
-# 9 разрабоать метод валидации вводимых данных
+    # 8 сделать ввод данных от пользователя
+    # 9 разрабоать метод валидации вводимых данных
